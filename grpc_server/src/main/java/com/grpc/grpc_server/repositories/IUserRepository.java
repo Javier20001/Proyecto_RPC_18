f6f9886bd9baf1d7b8fc08227aa7672a434e6a82
Matias Sayut <matiassayut@gmail.com>
2024-09-10 22:35:57 -0300

    Create/Modify Repositories

--- grpc_server/src/main/java/com/grpc/grpc_server/repositories/IUserRepository.java
+++ grpc_server/src/main/java/com/grpc/grpc_server/repositories/IUserRepository.java
@@ -1,14 +1,18 @@
 package com.grpc.grpc_server.repositories;
 
+import com.grpc.grpc_server.entities.Tienda;
 import com.grpc.grpc_server.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository("userRepository")
 public interface IUserRepository extends JpaRepository<User, Integer> {
 
     public abstract Optional<User> findByUsername(String username);
+
+    public abstract List<User> findByTienda(Tienda tienda);
 
 }
