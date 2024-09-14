2a44432a716599ad335664afc9d14d19c2b32677
Matias Sayut <matiassayut@gmail.com>
2024-09-14 10:49:27 -0300

    Add CRUD for Tienda

--- grpc_server/src/main/java/com/grpc/grpc_server/repositories/ITiendaRepository.java
+++ grpc_server/src/main/java/com/grpc/grpc_server/repositories/ITiendaRepository.java
@@ -4,11 +4,14 @@ import com.grpc.grpc_server.entities.Tienda;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository("tiendaRepository")
 public interface ITiendaRepository extends JpaRepository<Tienda, Integer> {
 
     public abstract Optional<Tienda> findByCodigo(String codigo);
+
+    public abstract List<Tienda> findAllByHabilitada(boolean habilitada);
 
 }
