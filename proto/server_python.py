14078303a3525a5f8cd08902f00b0988ee35cfec
Carlos341988 <magulon26@gmail.com>
2024-09-17 10:12:20 -0300

    user con errores

--- proto/server_python.py
+++ proto/server_python.py
@@ -2,13 +2,20 @@ import sys
 import grpc
 from flask import Flask, jsonify, request
 from tienda_service_client_grpc import TiendaClient
-import tienda_service_pb2
+from user_service_client_grpc import UserClient 
+#import tienda_service_pb2
+import user_service_pb2
+
+
+
 
 app = Flask(__name__)
 
 
 channel = grpc.insecure_channel('localhost:9090')
 tiendaclient = TiendaClient(channel)
+user_client = UserClient(channel)
+
 
 @app.route('/tiendas', methods=['GET'])
 def gettiendas():
@@ -178,6 +185,35 @@ def delete_tienda(tienda_id):
             return jsonify({'error': 'No se pudo deshabilitar la tienda'}), 500
     except grpc.RpcError as e:
         return jsonify({'error': str(e.details())}), 500
+
+
+
+#---------------------------------------------------------------------------------------------------------------------------
+
+@app.route('/usuarios', methods=['POST'])
+def add_user():
+    try:
+        # Obtener datos del cuerpo de la solicitud
+        data = request.json
+        username = data.get('username')
+        password = data.get('password')
+        tienda_id = int(data.get('tienda'))  # Obtener el valor de tienda
+        nombre = data.get('nombre')
+        apellido = data.get('apellido')
+        rol = data.get('rol')
+        habilitado = data.get('habilitado')
+
+       # Llamar al método add_user del cliente gRPC
+        user = user_client.add_user(username, password, tienda_id, nombre, apellido, rol, habilitado)
+
+        if user:
+            return jsonify(user), 201
+        else:
+            return jsonify({'error': 'Unable to add user'}), 500
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+
 
 if __name__ == '__main__':
     app.run(port=8081)
