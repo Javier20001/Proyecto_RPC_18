a616421de9a8eb7df647644aabbedc6dad57dfd5
Matias Sayut <matiassayut@gmail.com>
2024-10-03 10:45:15 -0300

    Copy entities

---
+++ SpringBootForKafka/SpringBooProvider/src/main/java/com/kafka/provider/entities/Tienda.java
@@ -0,0 +1,39 @@
+package com.kafka.provider.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "tienda")
+@Data
+@NoArgsConstructor
+public class Tienda {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "codigo", nullable = false, length = 100)
+    private String codigo;
+
+    @Column(name = "provincia", nullable = false, length = 100)
+    private String provincia;
+
+    @Column(name = "ciudad", nullable = false, length = 100)
+    private String ciudad;
+
+    @Column(name = "direccion", nullable = false, length = 150)
+    private String direccion;
+
+    @Column(name = "habilitada", nullable = false)
+    private Boolean habilitada;
+
+    public Tienda(String codigo, String provincia, String ciudad, String direccion, Boolean habilitada) {
+        this.codigo = codigo;
+        this.provincia = provincia;
+        this.ciudad = ciudad;
+        this.direccion = direccion;
+        this.habilitada = habilitada;
+    }
+}
