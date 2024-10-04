b7471e2b1a828b7189f7f2eabc7cad6bbc6ae310
Matias Sayut <matiassayut@gmail.com>
2024-10-04 10:46:55 -0300

    Punto 7

--- SpringBootForKafka/SpringBooProvider/src/main/java/com/kafka/provider/repository/IOrdenDeCompraRepository.java
+++ SpringBootForKafka/SpringBooProvider/src/main/java/com/kafka/provider/repository/IOrdenDeCompraRepository.java
@@ -4,6 +4,11 @@ import com.kafka.provider.entities.OrdenDeCompra;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository("OrdenDeCompraRepository")
 public interface IOrdenDeCompraRepository extends JpaRepository<OrdenDeCompra, Integer> {
+
+    Optional<OrdenDeCompra> findByOrdenDeDespacho_Id(int ordenDeDespachoId);
+
 }
