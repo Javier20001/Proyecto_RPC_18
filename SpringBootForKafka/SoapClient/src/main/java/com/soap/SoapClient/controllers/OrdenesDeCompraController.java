b26b7544702ade11f86fd7f91a64ae967b44363c
Matias Sayut <matiassayut@gmail.com>
2024-10-23 22:29:54 -0300

    Punto 2 Soap Client

--- SpringBootForKafka/SoapClient/src/main/java/com/soap/SoapClient/controllers/OrdenesDeCompraController.java
+++ SpringBootForKafka/SoapClient/src/main/java/com/soap/SoapClient/controllers/OrdenesDeCompraController.java
@@ -1,8 +1,6 @@
 package com.soap.SoapClient.controllers;
 
-import com.example.consumingwebservice.wsdl.GetAllOrdenesDeCompraResponse;
-import com.example.consumingwebservice.wsdl.GetOrdenesDeCompraFiltradasRequest;
-import com.example.consumingwebservice.wsdl.GetOrdenesDeCompraFiltradasResponse;
+import com.example.consumingwebservice.wsdl.*;
 import com.soap.SoapClient.services.OrdenesClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -20,7 +18,7 @@ public class OrdenesDeCompraController {
     }
 
     //este es el body: (Aclaracion: el fechaInicio y fechaFin, filtraria la fecha de solicitud de las Ordenes de compra, no la fecha de recepcion.)
-    /*
+    /* se pueden enviar todos los datos vacios
     {
         "codigoProducto": "PROD001",
         "estado": "ACEPTADA",
@@ -32,6 +30,53 @@ public class OrdenesDeCompraController {
     @PostMapping("/filtrar")
     public GetOrdenesDeCompraFiltradasResponse filtrarOrdenes(@RequestBody GetOrdenesDeCompraFiltradasRequest request) {
         return ordenesClient.filtrarOrdenes(request);
+    }
+
+    /* se pueden enviar todos los datos vacios menos usuarioId
+    {
+        "usuarioId": 1,
+        "codigoProducto": "PROD001",
+        "nombre": "nombre 1",
+        "estado": "ACEPTADA",
+        "fechaInicio": "2023-01-01",
+        "fechaFin": "2024-12-31",
+        "tiendaId": 1
+    }
+     */
+    @PostMapping("/addFiltro")
+    public AddFiltroResponse addFiltro(@RequestBody AddFiltroRequest request) {
+        return ordenesClient.addFiltro(request);
+    }
+
+    @GetMapping("/filtros/{usuarioId}")
+    public GetFiltrosByUsuarioResponse getFiltrosByUsuario(@PathVariable int usuarioId) {
+        return ordenesClient.getFiltrosByUsuario(usuarioId);
+    }
+
+    @GetMapping("/ordenesByFiltro/{filtroId}")
+    public GetAllOrdenesByFiltroIdResponse getAllOrdenesByFiltroId(@PathVariable int filtroId) {
+        return ordenesClient.getAllOrdenesByFiltroId(filtroId);
+    }
+
+    /* Se pueden enviar todos los datos vacios menos filtroId
+    {
+        "filtroId": 4,
+        "codigoProducto": "PROD001",
+        "nombre": "nombre 1",
+        "estado": "ACEPTADA",
+        "fechaInicio": "2023-01-01",
+        "fechaFin": "2024-12-31",
+        "tiendaId": 1
+    }
+     */
+    @PutMapping("/updateFiltro")
+    public UpdateFiltroByIdResponse updateFiltro(@RequestBody UpdateFiltroByIdRequest request) {
+        return ordenesClient.updateFiltroById(request);
+    }
+
+    @DeleteMapping("/deleteFiltro/{filtroId}")
+    public DeleteFiltroByIdResponse deleteFiltro(@PathVariable int filtroId) {
+        return ordenesClient.deleteFiltroById(filtroId);
     }
 
 }
--- SpringBootForKafka/SoapClient/src/main/java/com/soap/SoapClient/services/OrdenesClient.java
+++ SpringBootForKafka/SoapClient/src/main/java/com/soap/SoapClient/services/OrdenesClient.java
@@ -1,9 +1,6 @@
 package com.soap.SoapClient.services;
 
-import com.example.consumingwebservice.wsdl.GetAllOrdenesDeCompraRequest;
-import com.example.consumingwebservice.wsdl.GetAllOrdenesDeCompraResponse;
-import com.example.consumingwebservice.wsdl.GetOrdenesDeCompraFiltradasRequest;
-import com.example.consumingwebservice.wsdl.GetOrdenesDeCompraFiltradasResponse;
+import com.example.consumingwebservice.wsdl.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.ws.client.core.support.WebServiceGatewaySupport;
@@ -38,5 +35,72 @@ public class OrdenesClient extends WebServiceGatewaySupport {
 
         return response;
     }
+
+    public AddFiltroResponse addFiltro(AddFiltroRequest request) {
+        log.info("Adding filtro with request: {}", request);
+
+        AddFiltroResponse response = (AddFiltroResponse) getWebServiceTemplate()
+                .marshalSendAndReceive("http://localhost:8088/ws/ordenes", request,
+                        new SoapActionCallback(
+                                "http://www.example.com/ordenes/AddFiltroRequest"));
+
+        return response;
+    }
+
+    public GetFiltrosByUsuarioResponse getFiltrosByUsuario(int usuarioId) {
+        GetFiltrosByUsuarioRequest request = new GetFiltrosByUsuarioRequest();
+        request.setUsuarioId(usuarioId);
+
+        log.info("Requesting filtros for usuarioId: {}", usuarioId);
+
+        GetFiltrosByUsuarioResponse response = (GetFiltrosByUsuarioResponse) getWebServiceTemplate()
+                .marshalSendAndReceive("http://localhost:8088/ws/ordenes", request,
+                        new SoapActionCallback(
+                                "http://www.example.com/ordenes/GetFiltrosByUsuarioRequest"));
+
+        return response;
+    }
+
+    public GetAllOrdenesByFiltroIdResponse getAllOrdenesByFiltroId(int filtroId) {
+        GetAllOrdenesByFiltroIdRequest request = new GetAllOrdenesByFiltroIdRequest();
+        request.setFiltroId(filtroId);
+
+        log.info("Requesting ordenes for filtroId: {}", filtroId);
+
+        GetAllOrdenesByFiltroIdResponse response = (GetAllOrdenesByFiltroIdResponse) getWebServiceTemplate()
+                .marshalSendAndReceive("http://localhost:8088/ws/ordenes", request,
+                        new SoapActionCallback(
+                                "http://www.example.com/ordenes/GetAllOrdenesByFiltroIdRequest"));
+
+        return response;
+    }
+
+    public UpdateFiltroByIdResponse updateFiltroById(UpdateFiltroByIdRequest request) {
+        log.info("Updating filtro with request: {}", request);
+
+        UpdateFiltroByIdResponse response = (UpdateFiltroByIdResponse) getWebServiceTemplate()
+                .marshalSendAndReceive("http://localhost:8088/ws/ordenes", request,
+                        new SoapActionCallback(
+                                "http://www.example.com/ordenes/UpdateFiltroByIdRequest"));
+
+        return response;
+    }
+
+    public DeleteFiltroByIdResponse deleteFiltroById(int filtroId) {
+        DeleteFiltroByIdRequest request = new DeleteFiltroByIdRequest();
+        request.setFiltroId(filtroId);
+
+        log.info("Deleting filtro with filtroId: {}", filtroId);
+
+        DeleteFiltroByIdResponse response = (DeleteFiltroByIdResponse) getWebServiceTemplate()
+                .marshalSendAndReceive("http://localhost:8088/ws/ordenes", request,
+                        new SoapActionCallback(
+                                "http://www.example.com/ordenes/DeleteFiltroByIdRequest"));
+
+        return response;
+    }
+
+
+
 
 }
